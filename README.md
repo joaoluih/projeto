26ba9aa5d72a15a8d4a9158a5cdea99414029c01
joaoluih <141836645+joaoluih@users.noreply.github.com>
2023-09-11 11:56:52 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # projeto
+Estrutura do projeto final
---
+++ template/index.js
@@ -0,0 +1,81 @@
+
+// Definições básicas
+let continuar = true
+let dados = []
+
+
+// Função para cadastro
+
+function cadastrar() {
+	let nome = prompt("Diga o nome do seu abrigo")
+	let localizaçao = prompt("Diga a locaçização do seu abrigo")
+	let contato = prompt("Deixe o numero ou email de contato do abrigo")
+	let abrigadosMaximo = Number(prompt("Qual o numero maximo de pessoas que o abrigo pode atenter?"))
+	let abrigadosAtual = Number(prompt("Qual o numero de pessoas atual no abrigo?"))
+
+	
+	const abrigos = {
+		nomes: nome,
+		localizaçoes: localizaçao,
+		contatos: contato,
+		capacidadeMaxima: abrigadosMaximo,
+		capacidadeAtual:  abrigadosAtual,
+	} 
+	dados.push(abrigos)
+}
+console.log(dados)
+
+
+// Função para listar
+function listar(dados) {
+	for(let i = 0; i < dados.length; i++ ){
+		console.log(`Nome | ${dados[i].nomes} | Locaçização |  ${dados[i].localizaçoes} | contato | ${dados[i].contatos} | capacidadeMaxima | ${dados[i].capacidadeMaxima} Pessoas | CapacidadeAtual |  ${dados[i].capacidadeAtual} Pessoas	`)
+	}
+}
+
+
+// Função para buscar
+function buscar() {
+    let busca = prompt("Diga o Abrigo")
+
+	
+}
+
+
+// Função para sair
+function sair() {
+	continuar = false
+	console.log("Tchau! Para reiniciar, atualize a aba do navegador.")
+}
+
+
+// Loop para perguntar ao usuário
+while (continuar) {
+	// Formatar pergunta
+	let pergunta  = "ABRIGOS TEMPORÁRIOS\n"
+		pergunta += "-------------------------\n"
+		pergunta += "Escolha uma opção:\n"
+		pergunta += "1. Cadastrar abrigo\n"
+		pergunta += "2. Listar abrigos\n"
+		pergunta += "3. Procurar abrigo\n"
+		pergunta += "4. Sair"
+
+	// Guardar resposta
+	let resposta = Number(prompt(pergunta))
+
+	// Identificar resposta e chamar função
+	switch (resposta) {
+		case 1:
+			cadastrar()
+			break
+		case 2:
+			listar(dados)
+			break
+		case 3:
+			buscar(dados)
+			break
+		case 4:
+			sair()
+			break
+	}
+}
